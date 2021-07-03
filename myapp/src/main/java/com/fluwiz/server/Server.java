fae370ba8b6dd3977d2d98edaadc64235c53ba44
ksvraja <ksvraja@xthink>
2021-07-03 22:21:20 +0530

    Simplified dependency and Main Class

---
+++ myapp/src/main/java/com/fluwiz/server/Server.java
@@ -0,0 +1,11 @@
+package com.fluwiz.server;
+
+import com.zitlab.palmyra.server.FluwizServer;
+
+public class Server {
+
+	public static void main(String[] args) throws Exception {
+		FluwizServer.start();
+	}
+
+}
