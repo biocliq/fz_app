9353e63d55fe4e4d1927b8351c96d52fa5f3dae4
ksvraja <ksvraja@gmail.com>
2021-07-04 22:44:19 +0530

    dependency updated

--- myapp/src/main/java/com/fluwiz/server/Server.java
+++ myapp/src/main/java/com/fluwiz/server/Server.java
@@ -1,11 +1,11 @@
 package com.fluwiz.server;
 
-import com.zitlab.palmyra.server.FluwizServer;
+import com.biocliq.fluwiz.server.FluwizServer;
 
 public class Server {
 
 	public static void main(String[] args) throws Exception {
-		FluwizServer.start();
+		FluwizServer.start(6060);
 	}
 
 }
